81ccc3aaec2a5e752d82532ea8a70bf65f893eb8
nchunguye <nchunguyeshaban@gmail.com>
2026-06-17 11:24:22 +0300

    Added user registration page

---
+++ register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Registration</title>
+</head>
+<body>
+    <h2>User Registration</h2>
+
+    <form method="post">
+        Username:
+        <input type="text" name="username"><br><br>
+
+        Email:
+        <input type="email" name="email"><br><br>
+
+        Password:
+        <input type="password" name="password"><br><br>
+
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
