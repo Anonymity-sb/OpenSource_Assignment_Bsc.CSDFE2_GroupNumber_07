81874deacf99e0a032a23189e50802a2322fec57
nchunguye <nchunguyeshaban@gmail.com>
2026-06-17 11:34:12 +0300

    addedd incident feature

---
+++ incidents.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search Incident</title>
+</head>
+<body>
+    <h2>Search Security Incident</h2>
+
+    <form method="get">
+        Incident ID:
+        <input type="number" name="incident_id">
+        <input type="submit" value="Search">
+    </form>
+</body>
+</html>
