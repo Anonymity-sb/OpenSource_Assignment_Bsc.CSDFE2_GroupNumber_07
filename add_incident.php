cd4feea8fb9636d9e632f988340d0853ce31bae4
nchunguye <nchunguyeshaban@gmail.com>
2026-06-17 11:26:56 +0300

    Added incidentbregistration form

---
+++ add_incident.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Incident</title>
+</head>
+<body>
+    <h2>Record Security Incident</h2>
+
+    <form method="post">
+        Incident Title:
+        <input type="text" name="title"><br><br>
+
+        Description:
+        <textarea name="description"></textarea><br><br>
+
+        Incident Date:
+        <input type="date" name="incident_date"><br><br>
+
+        <input type="submit" value="Save Incident">
+    </form>
+</body>
+</html>
